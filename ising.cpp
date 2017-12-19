e8a5c4b227312d6720bd8333154b3e0fb0b1c724
Patrick Louden <plouden@nd.edu>
2017-12-19 11:22:41 -0500

    potts

--- ising.cpp
+++ ising.cpp
@@ -4,17 +4,91 @@
 
 #include "ising.hpp"
 #include <iostream>
+#include <vector>
+#include <math.h>
 using namespace std;
 
+void MonteCarloSim(Lattice myLattice,int nCycles) {
+  int nSpins = pow(myLattice.getSideLength(),2);
+  int sampleTime = 1;
+  int attemptedMoves = 0;
+  int acceptedMoves = 0; 
+  float oldEnergy = myLattice.calcTotalEnergy();
+  float oldMagnetization = myLattice.calcMagnetization();
+  float newEnergy = 0.0;
+  float newMagnetization = 0.0;
 
+  float deltaE = 0.0;
+  float deltaS = 0.0;
+
+  //Loop over the designated number of MCSweeps
+  for (int iCycle = 0; iCycle < nCycles; iCycle++) {
+    
+    myLattice.accumulateStats(oldEnergy,oldMagnetization);
+    
+    //For each sweep, loop through entire lattice
+    for (int idex = 0; idex < myLattice.getSideLength(); idex++){
+      for (int jdex = 0; jdex < myLattice.getSideLength(); jdex++){	
+	
+	attemptedMoves = attemptedMoves + 1;
+
+	myLattice.flipSpin(idex,jdex);
+	deltaE = myLattice.calcDifferenceInEnergy(idex,jdex);
+	deltaS = myLattice.calcDifferenceInMagnetization(idex,jdex);
+	
+	newEnergy = oldEnergy + deltaE;
+	newMagnetization = oldMagnetization + deltaS;
+	  
+	if (newEnergy > oldEnergy){
+	  double randNum = (double)rand()/(double)RAND_MAX;
+	  // compare new energy to a thermal distribution
+	  if (randNum <= exp(-1.0/myLattice.getTemperature())*(newEnergy - oldEnergy)) {
+	    acceptedMoves = acceptedMoves + 1;
+	    oldEnergy = newEnergy;
+	    oldMagnetization = newMagnetization;
+	  }
+	  // new energy is too large when compared to a thermal distribution
+	  // flip spin again to go back to old configuration.
+	  else {
+	    myLattice.flipSpin(idex,jdex);
+	  }
+	}
+	// if the new energy configuration is lower than old, keep the move
+	else {
+	  acceptedMoves = acceptedMoves + 1;
+	  oldEnergy = newEnergy;
+	  oldMagnetization = newMagnetization;
+	}
+
+
+	//if (iCycle % (nCycles/1000) == 0) {
+	  //myLattice.printLattice();
+	  //std::cout << "energy of lattice = " << oldEnergy << endl;
+	  //std::cout << "magnetization of lattice = " << oldMagnetization << endl;
+	  //float percent = float(iCycle) / float(nCycles);
+	  //std::cout << "%complete = " << percent * 100.0  << endl;
+	//}
+	
+      } // idex loop
+    } // jdex loop
+
+  }// iCycle loop
+  
+}
 
 
 int main() {
   Lattice myLattice;
-
   myLattice.initializeLattice();
-  
-  std::cout << "hello!" << endl;
 
+  int nCycles;
+  std::cout << "Please enter the number of Monte Carlo Sweeps:" << endl;
+  std::cin >> nCycles;
+  
+  MonteCarloSim(myLattice, nCycles);
+
+  std::cout << "Printing out lattice after all MC steps are completed" << endl;
+  myLattice.printLattice();
+  
   return 0;
 }
--- ising.hpp
+++ ising.hpp
@@ -1,6 +1,7 @@
 #include <vector>
 #include <string>
 #include <iostream>
+#include <math.h>
 using namespace std;
 
 class Lattice {
@@ -10,7 +11,8 @@ public:
   int sideLength;
   int numSpins;
   float spinMoment;
-
+  float externField;
+  
   vector< vector<float> > matrix;
   vector<float> aveSpin;
   vector<float> latticeEnergy;
@@ -26,7 +28,20 @@ public:
   int getSideLength(void);
   void setSideLength(int newSideLength);
 
+  int getExternField(void);
+  void setExternField(float newField);
+  
+  // Utility functions of the lattice.
   void initializeLattice(void);
+  void printLattice(void);
+  float calcTotalEnergy(void);
+  float calcMagnetization(void);
+  int getRandomCoord(void);
+  void flipSpin(int idex, int jdex);
+  float calcDifferenceInEnergy(int idex, int jdex);
+  float calcDifferenceInMagnetization(int idex, int jdex);
+  
+  void accumulateStats(float energy, float magnetization);
 
   
 };
@@ -40,12 +55,11 @@ int Lattice::getTemperature(void) {
 }
 
 void Lattice::setTemperature(float newTemp) {
-  if (newTemp >= 0.0) {
+  if (newTemp >= 0.0) 
     temperature = newTemp;
-  }
-  else {
+  
+  else 
     std::cout << "Temperature must be >= 0.0" << endl;
-  }
 }
 
 float Lattice::getCoupling(void) {
@@ -61,11 +75,28 @@ int Lattice::getSideLength(void) {
 }
 
 void Lattice::setSideLength(int newSideLength) {
-  sideLength = newSideLength;
+  if (newSideLength > 0) {
+    sideLength = newSideLength;
+  }
+  else
+    std::cout << "Side length must be > 0." << endl; 
+}
+
+int Lattice::getExternField(void){
+  return externField;
+}
+
+void Lattice::setExternField(float newField){
+  externField = newField;
 }
 
 
-
+// Utility Functions
+/*
+initializeLattice will ask the user to set the temperature, coupling 
+constant, and side length (num of spins to a side).
+The function then sets the parameters and builds the lattice.
+*/
 void Lattice::initializeLattice(void) {
   string localTemp;
   std::cout << "Please enter the temperature:" << endl;
@@ -85,8 +116,171 @@ void Lattice::initializeLattice(void) {
   int ilocalSideLength = std::atoi((localSideLength).c_str());
   setSideLength(ilocalSideLength);
 
-  
+  // Next we will initialize the matrix (lattice) to all spin up
+  for (int i = 0; i < ilocalSideLength; i++) {
+    vector<float> row;
+    for (int j = 0; j < ilocalSideLength; j++) {
+      row.push_back(1.0);
+    }
+    matrix.push_back(row);
+  }
+
+  // Print out values that we have set
+  //std::cout << "Temp set to " << getTemperature() << endl;
+  //std::cout << "Coupling set to " << getCoupling() << endl;
+  //std::cout << "SideLength set to " << getSideLength() << endl;
+ 
 }
 
+
+void Lattice::printLattice(void) {
+  for (int i = 0; i < sideLength; i++) {
+    for (int j = 0; j < sideLength; j++) {
+      std::cout << matrix[i][j] << "\t";
+    }
+    std::cout << "\n";
+  }
+}
+
+
+float Lattice::calcTotalEnergy(void) {
+  float energySum = 0.0;
+  int idex = 0;
+  int jdex = 0;
+  /*
+    There are four pairs of energies that need to be computed for each i,j pair.
+    for a given i,j, there are 
+    (i,j) <-> (i-1,j)     (i,j) <-> (i,j-1)
+    (i,j) <-> (i+1,j)     (i,j) <-> (i,j+1)
+   */
+
+  
+  for (int i = 0; i < sideLength; i++) {
+    for (int j = 0; j < sideLength; j++) {
+      
+      // (i,j) <-> (i-1,j)
+      if ( i-1 < 0) {
+	idex = i - 1 + sideLength;
+	energySum = energySum - (matrix[i][j] * matrix[idex][j]);
+      }
+      else if ( i-1 >= 0) {
+	  idex = i - 1;
+	  energySum = energySum - (matrix[i][j] * matrix[idex][j]);
+	}
+
+      // (i,j) <-> (i+1,j)
+      if ( i+1 == sideLength) {
+	idex = i + 1 - sideLength;
+	energySum = energySum - (matrix[i][j] * matrix[idex][j]);
+      }
+      else if (i+1 < sideLength) {
+	  idex = i + 1;
+	  energySum = energySum - (matrix[i][j] * matrix[i][j]);
+	}
+
+      // (i,j) <-> (i,j-1)
+      if (j-1 < 0) {
+	jdex = j - 1 + sideLength;
+	energySum = energySum - (matrix[i][j] * matrix[i][jdex]);
+      }
+      else if (j-1 >= 0) {
+	  jdex = j - 1;
+	  energySum = energySum - (matrix[i][j] * matrix[i][jdex]);
+	}
+
+      // (i,j) <-> (i,j+1)
+      if (j+1 == sideLength) {
+	jdex = j + 1 - sideLength;
+	energySum = energySum - (matrix[i][j] * matrix[i][jdex]);
+      }
+      else if (j+1 < sideLength){
+	jdex = j;
+	energySum = energySum - (matrix[i][j] * matrix[i][jdex]);
+      }
+	
+    }
+  }
+  energySum = energySum * (coupling / 2.0);
+  return energySum;
+}
+
+float Lattice::calcMagnetization(void) {
+  float magnetization = 0.0;
+  for (int i = 0; i < sideLength; i++) {
+    for (int j = 0; j < sideLength; j++) {
+      magnetization = magnetization + matrix[i][j];
+    }
+  }
+  return (magnetization / pow(sideLength, 2.0));
+}
+
+int Lattice::getRandomCoord(void) {
+  double val = (double)rand()/(double)RAND_MAX;
+  return (val*sideLength);
+}
+
+void Lattice::flipSpin(int idex, int jdex) {
+  matrix[idex][jdex] = -1.0 * matrix[idex][jdex];
+  std::cout << matrix[idex][jdex] << endl;
+}
+
+
+float Lattice::calcDifferenceInEnergy(int idex, int jdex) {
+  float randSpinE = 0.0;
+  int i = 0;
+  int j = 0;
+  /*
+    This time we will have to be more careful with neighboring interactions.
+  */
+
+  if ( idex-1 < 0){
+    i = idex - 1 + sideLength;
+    randSpinE = randSpinE - (matrix[idex][jdex] * matrix[i][jdex]);
+  }
+  else if (idex-1 >= 0) {
+    randSpinE = randSpinE - (matrix[idex][jdex] * matrix[idex-1][jdex]);
+  }
+
+  if (idex+1 == sideLength) {
+    i = idex + 1 - sideLength;
+    randSpinE = randSpinE - (matrix[idex][jdex] * matrix[i][jdex]);
+  }
+  else if (idex+1 < sideLength) {
+    randSpinE = randSpinE - (matrix[idex][jdex] * matrix[idex+1][jdex]);
+  }
+
+   if ( jdex-1 < 0){
+    j = jdex - 1 + sideLength;
+    randSpinE = randSpinE - (matrix[idex][jdex] * matrix[idex][j]);
+  }
+  else if (jdex-1 >= 0) {
+    randSpinE = randSpinE - (matrix[idex][jdex] * matrix[idex][jdex-1]);
+  }
+
+  if (jdex+1 == sideLength) {
+    j = jdex + 1 - sideLength;
+    randSpinE = randSpinE - (matrix[idex][jdex] * matrix[idex][j]);
+  }
+  else if (jdex+1 < sideLength) {
+    randSpinE = randSpinE - (matrix[idex][jdex] * matrix[idex][jdex+1]);
+  }
+
+  randSpinE = randSpinE * 2.0 * coupling;
+  return randSpinE;
+}
+
+
+float Lattice::calcDifferenceInMagnetization(int idex, int jdex) {
+  float newMagnetization;
+  newMagnetization = 2.0 * matrix[idex][jdex] / pow(sideLength,2);
+
+  return newMagnetization;
+}
+ 
+
+void Lattice::accumulateStats(float energy, float magnetization) {
+  aveSpin.push_back(magnetization);
+  latticeEnergy.push_back(energy); 
+}
 
 
